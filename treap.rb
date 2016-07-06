96a4b7ebbc4c65779d82cb3ac443051a5cea92d6
lastromanticx <giladbarkan@live.com>
2016-07-05 22:50:54 -0400

    added shift_r, changed treap to t

--- treap.rb
+++ treap.rb
@@ -8,16 +8,16 @@ class T
     @size = 1
   end
 
-  def self.size(treap)
-    if treap then treap.size else 0 end
+  def self.size(t)
+    if t then t.size else 0 end
   end
 
-  def self.update(treap)
-    if treap
-      treap.size = 1 + self.size(treap.left) + self.size(treap.right)
+  def self.update(t)
+    if t
+      t.size = 1 + self.size(t.left) + self.size(t.right)
     end
     
-    treap
+    t
   end
 
   def self.merge(a,b)
@@ -32,31 +32,31 @@ class T
     end
   end
 
-  def self.split(treap,i)
-    if not treap then return [nil,nil] end
-    if i <= self.size(treap.left)
-      u, treap.left = self.split(treap.left, i)
-      [u, self.update(treap)]
+  def self.split(t,i)
+    if not t then return [nil,nil] end
+    if i <= self.size(t.left)
+      u, t.left = self.split(t.left, i)
+      [u, self.update(t)]
     else
-     treap.right, u = self.split(treap.right, i - self.size(treap.left) - 1)
-      [self.update(treap), u]
+     t.right, u = self.split(t.right, i - self.size(t.left) - 1)
+      [self.update(t), u]
     end
   end
 
-  def self.insert(treap, i, value)
-    left, right = self.split(treap,i)
+  def self.insert(t, i, value)
+    left, right = self.split(t,i)
 
     u = self.new(value)
     self.merge(self.merge(left,u), right)
   end
 
-  def self.erase(treap,i)
-    left, right = self.split(treap,i + 1)
+  def self.erase(t,i)
+    left, right = self.split(t,i + 1)
     left, u = self.split(left, i)
     [self.merge(left,right), u]
   end
 
- # shifts elements between [l,r] to position i
+ # shifts elements between [l,r] left to position i
   def self.shift_l t,l,r,i
     a,c = split t,r + 1
     a,lr = split a,l
@@ -64,6 +64,17 @@ class T
 
     t = merge(merge(a,lr), merge(b,c))
     
+    t
+  end
+
+ # shifts elements between [l,r] right to position i
+  def self.shift_r t,l,r,i
+    a,c = split t,l
+    lr,c = split c,r - l + 1 
+    b,c = split c,i - r + 1
+
+    t = merge(merge(a,b), merge(lr,c))
+
     t
   end
 end
